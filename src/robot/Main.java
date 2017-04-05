8730ff238b1e9f60c4a5feb1e5b939d25c93ef3b
superdarktu <695445293@qq.com>
2017-04-05 15:28:49 +0800

    the begin

--- src/robot/Main.java
+++ src/robot/Main.java
@@ -12,6 +12,8 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
+import Listener.KeyboardListener;
+
 /**
  * @author tumingjun
  *
@@ -21,12 +23,16 @@ public class Main {
 	public static void main(String[] args) throws AWTException {
 
 		Robot robot = new Robot(); //创建一个robot对象
+	//	KeyboardListener kl = new KeyboardListener();
+	//	kl.run();
 		robot.delay(2000);
 		for(int i=0;i<10;i++){
-			robot.keyPress(KeyEvent.VK_X);
-			robot.keyRelease(KeyEvent.VK_X);
+		//	robot.keyPress(KeyEvent.VK_X);
+		//	robot.keyRelease(KeyEvent.VK_X);
+			System.out.println("11111111");
 			robot.delay(2000);
 		}
-	}
+	} 
+	
 
 }
