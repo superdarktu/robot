183ec596476de40d47d3711904a1351c18de33be
tumingjun <tumj63@visionvera.com>
2019-01-14 10:55:47 +0800

    no message

---
+++ src/model/Position.java
@@ -0,0 +1,16 @@
+package model;
+
+public abstract class Position {
+
+    // 钓鱼控杆时左边提示位
+    public  int FISHING_LEFT_X;
+    public  int FISHING_LEFT_Y;
+
+    //钓鱼控杆时右边提示位
+    public  int FISHING_RIGHT_X;
+    public  int FISHING_RIGHT_Y;
+
+    //钓鱼咬钩时提示位
+    public static int FISHING_X ;
+    public static int FISHING_Y ;
+}
